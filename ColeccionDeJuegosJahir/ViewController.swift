30fad9eb3a7aaffef0e017f1242a3b2c234ee97a
Jahir <jahir.larico@tecsup.edu.pe>
2022-10-17 12:53:25 -0700

    Initial Commit

---
+++ ColeccionDeJuegosJahir/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ColeccionDeJuegosJahir
+//
+//  Created by Jahir Larico on 10/17/22.
+//  Copyright © 2022 Jahir Larico. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
